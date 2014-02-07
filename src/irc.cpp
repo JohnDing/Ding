168576d6fadae3223737749bbad7747861bc9a74
unknown <bitcoincds@gmail.com>
2014-02-06 21:54:56 -0500

    changes

--- src/irc.cpp
+++ src/irc.cpp
@@ -292,16 +292,16 @@ void ThreadIRCSeed2(void* parg)
                 Send(hSocket, strprintf("NICK %s\r", strMyName.c_str()).c_str());
             }
         }
-        
+
         if (fTestNet) {
-            Send(hSocket, "JOIN #barcoin2TEST3\r");
-            Send(hSocket, "WHO #barcoin2TEST3\r");
+            Send(hSocket, "JOIN #ding2TEST3\r");
+            Send(hSocket, "WHO #ding2TEST3\r");
         } else {
-            // randomly join #barcoin00-#barcoin99
+            // randomly join #ding00-#ding99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #barcoin2%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #barcoin2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #ding2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #ding2%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
